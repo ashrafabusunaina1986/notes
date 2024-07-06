b755ae11f0989a759f8e97641ca85b0203a70de4
ashrafabusunaina1986 <101833519+ashrafabusunaina1986@users.noreply.github.com>
2024-07-06 22:16:41 +0300

    Update route.ts

--- src/app/api/get-note/route.ts
+++ src/app/api/get-note/route.ts
@@ -6,7 +6,7 @@ export const GET = async (req: NextRequest) => {
   try {
     await db();
     const notes = await Note.find();
-    if (notes) return NextResponse.json({ success: true, notes });
+    if (notes) return NextResponse.json({ success: true, notes:notes.reverse() });
     else return NextResponse.json({ success: false, message: "try again" });
   } catch (error: any) {
     return NextResponse.json({ success: false, message: error.message });
