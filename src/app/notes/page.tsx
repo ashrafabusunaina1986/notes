ada07b1d8778dcb92db2753f4c1926c767badcad
ashrafabusunaina1986 <101833519+ashrafabusunaina1986@users.noreply.github.com>
2024-07-06 17:20:04 +0300

    Update page.tsx

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -2,7 +2,7 @@ import Note_overview from "@/components/note-overview";
 import React from "react";
 async function getNotes() {
   try {
-    const apires = await fetch("https://notes-murex-ten/api/get-note",{
+    const apires = await fetch("http://notes-murex-ten/api/get-note",{
       method:'GET',
       cache:'no-store'
     });
